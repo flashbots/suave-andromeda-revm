5fedd6b05e6480fd9da9c95aed046bf6f378fe83
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2024-06-17 17:32:56 +0200

    Updates README and Andromeda.sol

--- examples/Andromeda.sol
+++ examples/Andromeda.sol
@@ -6,35 +6,42 @@ contract Andromeda {
     address public constant VOLATILESET_ADDR   = 0x0000000000000000000000000000000000040701;
     address public constant VOLATILEGET_ADDR   = 0x0000000000000000000000000000000000040702;
     address public constant RANDOM_ADDR        = 0x0000000000000000000000000000000000040703;
+    address public constant TDX_DCAP_VERIFY    = 0x0000000000000000000000000000000000040800;
     address public constant SHA512_ADDR        = 0x0000000000000000000000000000000000050700;
     address public constant DO_HTTP_REQUEST    = 0x0000000000000000000000000000000043200002;
     address public constant X509_GENERATE_ADDR = 0x0000000000000000000000000000000000070700;
 
     function volatileSet(bytes32 key, bytes32 value) public view {
-	bytes memory cdata = abi.encodePacked([key, value]);
+        bytes memory cdata = abi.encodePacked([key, value]);
         (bool success, bytes memory _out) = VOLATILESET_ADDR.staticcall(cdata);
-	_out;
-	require(success);
+        _out;
+        require(success);
     }
 
     function volatileGet(bytes32 key) public view returns (bytes32) {
         (bool success, bytes memory value) = VOLATILEGET_ADDR.staticcall(abi.encodePacked((key)));
-	require(success);
-	require(value.length == 32);
-	return abi.decode(value, (bytes32));
+        require(success);
+        require(value.length == 32);
+        return abi.decode(value, (bytes32));
     }
-    
+
     function attestSgx(bytes memory userdata) public view returns (bytes memory) {
         (bool success, bytes memory attestBytes) = ATTEST_ADDR.staticcall(userdata);
-	require(success);
-	return attestBytes;
+        require(success);
+        return attestBytes;
     }
 
     function localRandom() payable public returns (bytes32) {
         (bool success, bytes memory randomBytes) = RANDOM_ADDR.staticcall("");
-	require(success);
-	require(randomBytes.length == 32);
-	return bytes32(randomBytes);
+        require(success);
+        require(randomBytes.length == 32);
+        return bytes32(randomBytes);
+    }
+
+    function verifyQuote(bytes memory quote, string memory pckCertPem, string memory pckCrlPem, string memory tcbInfoJson, string memory qeIdentityJson) external returns (uint) {
+        (bool success, bytes memory status) = TDX_DCAP_VERIFY.staticcall("");
+        require(success);
+        return abi.decode(status, (uint));
     }
 
     function sha512(bytes memory data) public view returns (bytes memory) {
