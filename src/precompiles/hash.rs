049301c4dcbd2767693be4a176937b2ea69443ec
Nick Hynes <nick@escrin.org>
2024-07-18 07:27:20 +0200

    Apply rustfmt

--- src/precompiles/hash.rs
+++ src/precompiles/hash.rs
@@ -1,8 +1,7 @@
-use sha2::{Sha512, Digest};
 use revm::precompile::{
-    Precompile, PrecompileError, PrecompileResult, PrecompileWithAddress,
-    StandardPrecompileFn,
+    Precompile, PrecompileError, PrecompileResult, PrecompileWithAddress, StandardPrecompileFn,
 };
+use sha2::{Digest, Sha512};
 
 use crate::u64_to_address;
 
@@ -11,19 +10,16 @@ pub const SHA512: PrecompileWithAddress = PrecompileWithAddress::new(
     Precompile::Standard(hash_sha512 as StandardPrecompileFn),
 );
 
-const INVALID_INPUT: PrecompileError =
-    PrecompileError::CustomPrecompileError("Invalid input!");
-
+const INVALID_INPUT: PrecompileError = PrecompileError::CustomPrecompileError("Invalid input!");
 
 fn hash_sha512(input: &[u8], gas_limit: u64) -> PrecompileResult {
     let gas_used = 10000 as u64;
     if gas_used > gas_limit {
         return Err(PrecompileError::OutOfGas);
-    } 
+    }
     if input.len() == 0 {
         return Err(INVALID_INPUT);
     }
-    let output = Sha512::digest(input).to_vec(); 
+    let output = Sha512::digest(input).to_vec();
     Ok((gas_used, output))
-    
 }
